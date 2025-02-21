d3a45281a218575878625d2089840abac91bcd68
PolskiPolakPL <97120832+PolskiPolakPL@users.noreply.github.com>
2025-02-21 20:53:03 +0100

    Added Interactive Lightswitch

---
+++ Assets/PolskiPolakPL/_Scripts/LightSwitch.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class LightSwitch : MonoBehaviour
+{
+    [SerializeField] bool lightsON = true;
+    [SerializeField] List<Light> lights = new List<Light>();
+    Interactable interactionSource;
+    private void Start()
+    {
+        interactionSource = GetComponent<Interactable>();
+        interactionSource.OnInteraction += ToggleLight;
+    }
+    void ToggleLight()
+    {
+        lightsON = !lightsON;
+        foreach (Light light in lights)
+        {
+            light.enabled = lightsON;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        interactionSource.OnInteraction -= ToggleLight;
+    }
+}
